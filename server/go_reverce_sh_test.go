0fbf0e5f1661bc8dc614257dd47dbeeb6e98d3f3
geniusmaaakun <ao_masa_0610@yahoo.co.jp>
2022-07-10 06:58:24 +0900

    add code

--- server/go_reverce_sh_test.go
+++ server/go_reverce_sh_test.go
@@ -1,7 +1,14 @@
 package server
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestNewReverceSH(t *testing.T) {
-
+	grsh := NewGoReverSH("192.168.56.1", "8080")
+	if grsh == nil || reflect.ValueOf(grsh).IsNil() {
+		t.Log(grsh)
+		t.Error("GoReverSh constructor error")
+	}
 }
